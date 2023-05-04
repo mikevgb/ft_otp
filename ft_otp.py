d54a527558ef158657a0cc4d48492e7063f76401
mikevgb <75221782+mikevgb@users.noreply.github.com>
2023-05-04 11:11:37 +0200

    Add files via upload

--- ft_otp.py
+++ ft_otp.py
@@ -6,6 +6,7 @@ import time
 import base64
 import struct
 import hmac
+import math
 
 if sys.argv[1] == "-g":
     #open the original txt file, read and join them as one string
@@ -35,24 +36,54 @@ if sys.argv[1] == "-g":
 
 if sys.argv[1] == "-k":
     #open the file and read as bytes, store into secret
-    with open(sys.argv[2], "rb") as inputF:
+    with open(sys.argv[2], "r") as inputF:
         secret = inputF.read()
+    bytesSecret = secret.encode("utf-8")
+    b32Secret = base64.b32encode(bytesSecret)
     lenOtp = 6
-    otpInterval = 30
-    currentTime = int(time.time() / otpInterval)
-    while len(secret) % 8 != 0:
-        secret += b"="
-    key = base64.b32decode(secret)
+    otpSteps = 30
+    currentTime = math.floor(time.time() / otpSteps)    #generate steps from start of time and use floor
+                                                        #to round the number if necessary
     
-    for ix in range(-2, 3):
-        b = struct.pack(">q", currentTime + ix)
-        hm = hmac.HMAC(key, b, hashlib.sha1).digest()
-        offset = hm[-1] & 0x0F
-        truncatedHash = hm[offset:offset + 4]
-        code = struct.unpack(">L", truncatedHash)[-1]
-        code &= 0x7FFFFFFF
-        code %= 1000000
-    print(code)
+    msg = currentTime.to_bytes(8, byteorder='big')
+    key = base64.b32decode(b32Secret)
+    
+    hmac1 = hmac.new(key, msg, hashlib.sha1).digest()
+    
+    offset = hmac1[-1] & 0x0f
+    truncatedHash = hmac1[offset:offset + 4]
+    totp = int.from_bytes(truncatedHash, byteorder='big') & 0x7fffffff
+    totp %= 10 ** lenOtp
+    
+    print(totp)
+    # hash = hmac.new(bytes(secret, encoding="utf-8"), currentTime.to_bytes(length=8, byteorder="big"), hashlib.sha1, )
+    
+    # bitstring = bin(int(hash.hexdigest(), base=16)) #convert hast to binary string
+    # print("bitstring =", bitstring)
+    # last4bits = bitstring[-4:] #get last four bits
+    # print("last4bits =", last4bits)
+    # offset = int(last4bits, base=2) #use last4bits as offset
+    # print("offset =", offset)
+    # takeBits = bitstring[offset:offset * 8] #use the offset and get next 32bits of the string
+    # print("takeBits =", takeBits)
+    # code = str(int(takeBits, base=2)) #convert the taken bits to int
+    # print("code1 =", code)
+    # code = code[-lenOtp:] #get the last digits
+    # print(code)
+    
+    # while len(secret) % 8 != 0:
+    #     secret += b"="
+    # key = base64.b32decode(secret)
+    
+    # for ix in range(-2, 3):
+    #     b = struct.pack(">q", currentTime + ix)
+    #     hm = hmac.HMAC(key, b, hashlib.sha1).digest()
+    #     offset = hm[-1] & 0x0F
+    #     truncatedHash = hm[offset:offset + 4]
+    #     code = struct.unpack(">L", truncatedHash)[-1]
+    #     code &= 0x7FFFFFFF
+    #     code %= 1000000
+    # print(code)
     
     #currentTime = 0
 
@@ -77,6 +108,7 @@ if sys.argv[1] == "-k":
             # otpStr = str(otp).zfill(lenOtp)
             # print(otpStr)
 
+#ref https://totp.youngforest.me/
         
 #ref https://hackernoon.com/implementing-2fa-how-time-based-one-time-password-actually-works-with-python-examples-cm1m3ywt
 
